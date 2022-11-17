83265f08057f00dc1718b32837b585066abc7dc0
myportafoliobryantnzapp <bry.tandazo@gmail.com>
2022-11-17 18:02:13 -0500

    Clase3

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -14,20 +14,33 @@ import {Preferences } from '@capacitor/preferences';
 @Injectable({
   providedIn: 'root'
 })
+
 export class PhotoService {
 
   constructor() { }
+  // mod
+  public photos: UserPhoto[] = [];
 
 
 
   public async addNewToGallery() {
     // take a photo
-    const capturedPhoto = await Camera.getPhoto;({
+    const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
-    
-  });
-
+    });
+    // Mod
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+   
 }
+
+// mod
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
 }
